55f57ac689125a3e63a5aeeb80e0e14723406447
Thomas <thomasheinzergz@gmail.com>
2026-06-08 14:23:56 -0300

    Creacion de modelos y persistencia y un Modelo, commit con errores

---
+++ WinFormsApp1/Modelos/Cliente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using WinFormsApp1.Persistencia;
+
+namespace WinFormsApp1.Modelos
+{
+    internal class Cliente
+    {
+        public int Id { get; set; }
+        public string Dni { get; set; } = string.Empty;
+
+        public string TipoDocumento { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public string Localidad { get; set; } = string.Empty;
+        public List<Reserva> Reservas { get; set; } = new List<Reserva>();
+
+        public Cliente() { }
+
+        public Cliente(int id, string dni, string nombre, string apellido,
+                       string telefono)
+        {
+            Id = id;
+            Dni = dni;
+            Nombre = nombre;
+            Apellido = apellido;
+            Telefono = telefono;
+            Reservas = new List<Reserva>();
+        }
+
+        public override string ToString()
+        {
+            return $"[{Id}] {Apellido}, {Nombre} | DNI: {Dni} | Tel: {Telefono} | Email: {Email} | Localidad: {Localidad} ";
+        }
+    }
+}
---
+++ WinFormsApp1/Persistencia/pUsuario.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp1.Persistencia
+{
+    internal class pUsuario
+    {
+    }
+}
