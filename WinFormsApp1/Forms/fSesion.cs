0c2b61b9e3283b6967d7df910988477bf9d84cb7
Eric <ericsand2007@gmail.com>
2026-06-14 17:57:14 -0300

    Creacion de forms de login y de sesion

---
+++ WinFormsApp1/Forms/fSesion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WinFormsApp1.Modelos;
+
+namespace WinFormsApp1.Forms
+{
+    public partial class fSesion : Form
+    {
+        Usuario _usuario;
+        public fSesion()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
