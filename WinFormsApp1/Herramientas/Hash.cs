fdb103d61ea988ec5d139e4ca78efdc498f3a5e8
Thomas <thomasheinzergz@gmail.com>
2026-06-15 17:29:39 -0300

    Eliminacion de ByCrypt

--- WinFormsApp1/Herramientas/Hash.cs
+++ WinFormsApp1/Herramientas/Hash.cs
@@ -3,9 +3,9 @@ namespace WinFormsApp1.Herramientas
     public static class Hash
     {
         public static string HashPassword(string passwordPlano)
-            => BCrypt.Net.BCrypt.HashPassword(passwordPlano);
+            => passwordPlano;
 
         public static bool VerifyPassword(string passwordPlano, string hashGuardado)
-            => BCrypt.Net.BCrypt.Verify(passwordPlano, hashGuardado);
+            => passwordPlano == hashGuardado;
     }
 }
