e074317946b3afe3e408e2f8770d1cb6638cdb76
Gaston Valenzuela <gastonvalenzuela75@gmail.com>
2026-06-22 19:15:20 -0300

    Cambios Gaston

---
+++ WinFormsApp1/Forms/fGastosExtra/fAgregarGastosExtra.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp1.Forms.fGastosExtra
+{
+    public partial class fAgregarGastosExtra : Form
+    {
+        public fAgregarGastosExtra()
+        {
+            InitializeComponent();
+        }
+
+        private void btGuardarGE_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btCancelarGE_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
